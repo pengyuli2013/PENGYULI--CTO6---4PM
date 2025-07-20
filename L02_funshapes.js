1d60f750f28c169197c1ddb221afe67e3b8d1dfc
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-07-20 08:39:39 +0000

    Jul 20, 2025, 8:39 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -3,7 +3,7 @@ Task 1: Add the setup() function
 */
 function setup() {
     createCanvas(600,400);
-    background(255,)
+    background(255,0,0);
 }
 function draw() {
   // Challenge 1: An eye using ellipse() function
