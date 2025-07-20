605ad698370d040443d0bd05f4c98fedd75c7c70
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-07-20 09:13:15 +0000

    Jul 20, 2025, 9:13 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -13,7 +13,7 @@ function draw() {
   ellipse(100,100,100,100);
 
   // Challenge 3: A house using rect() function
-  rectangle(200,200,)
+  rectangle(200,200,100,200)
 
 
   // Challenge 4: A rocketship using triangle() function
