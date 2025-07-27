50e1d05b8b5423ee11ba33bb61b99a98ddc908a5
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-07-27 09:46:18 +0000

    Co-authored-by: Lyn101s <Lyn101s@users.noreply.github.com>

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -13,16 +13,33 @@ function draw() {
   // ellipse(100,100,100,100);
 
   // Challenge 3: A house using rect() function
-  rect(200,200,100,200,20,20,20,20);
+  // rect(200,200,100,200,20,20,20,20);
 
 
-  // Challenge 4: A rocketship using triangle() function
-  fill(255,0,0);
-  triangle(100,150,50,50);
+  // // Challenge 4: A rocketship using triangle() function
+  // fill(0,0,0);
+  // triangle(100,150,50,50);
 
   // Challenge 5: A square face using quad() function
+  //  quad(50, 50, 150, 70, 180, 150, 80, 130);
+  // quad(50,50,100,50,100,100,50,100);
+  fill(0,250,0);
+  stroke(0,255,0)
+  strokeWeight(10);
+  quad(50,162,86,150,50,138,14,150);
+  quad(120,150,180,130,180,170,120,170);
 
   // Challenge 6: Add some colours to the drawings in the above challenges
+  var r==0;
+  var g==0;
+  var b==0;
+  while(true) {
+    fill(r,0,0);
+    var r==r+1;
 
+  }
+  rect(50,)  
+
+  
   // Challenge 7: Create a funny Pokemon
 }
--- L03_patterns.js
+++ L03_patterns.js
@@ -1,11 +1,29 @@
+let colorValue=0
+
 function setup() {
     createCanvas(600, 400);
     background(220);
-    noLoop(); // Stops continuous drawing
+  //   noLoop(); // Stops continuous drawing
   }
-  
+  let spacing=90;
 function draw() {
   // Recap 1: Repeating Circles
+  // circle(75,200,100);
+  // circle(225,200,100);
+  // circle(375,200,100);
+  // circle(525,200,100);
+  for(let i =0; i < 4; i++) {
+    fill(colorValue%255);
+    circle(spacing+i*spacing,200,40);
+    colorValue +=1;
+    
+  
+  }
+
+
+
+
+
 
   // Task 1: Colour Gradient
 
