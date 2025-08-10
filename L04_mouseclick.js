53ba64f4fe89527a6677993a2709cebaef866f7a
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-08-10 09:05:13 +0000

    Aug 10, 2025, 9:05 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -30,5 +30,5 @@ function mousePressed(){
     shapeColour = 'red';
 }
 function mouseReleased(){
-    shapeColour = color(random(255)),random(255),random(255);
+    shapeColour = color(random(255),random(255),random(255));
 }
