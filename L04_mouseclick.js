79761246a9991ca5cbb60f1c885f78f73011d452
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-08-10 09:01:29 +0000

    Aug 10, 2025, 9:01 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,14 +1,34 @@
 // write your codes here
-
+// let shapeColour = 'blue';
 function setup()  {
-    createCanvas(400,400);
+    createCanvas(600,600);
     background(220);
 }
-
-function draw(){
-    for ( let o =0;o<5;o++){
-        fill(0,255%o,0);
-        circle(50+o*50, 50+o*50, 50);
-    }
+// let amount =5
+// let var = null
+// function draw(){
+//     for ( let o =0;o<amount;o++){
+//         var = 255/amount;
+//         fill(0,0+var,0);
+//         circle(50+o*50, 50+o*50, 50);
+//     }
     
+// }
+// function draw(){
+//     fill(shapeColour);
+//     circle(width/2,height/2,100);
+
+// }
+// function mousePressed(){
+//     shapeColour = 'red';
+// }
+// function mouseReleased(){
+//     //shapeColour = 'white';
+// }
+
+function mousePressed(){
+    shapeColour = 'red';
+}
+function mouseReleased(){
+    shapeColour = 'white';
 }
