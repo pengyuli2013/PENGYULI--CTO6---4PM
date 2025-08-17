bd4f6a1cab74220abebac956d0b6d74ba999b581
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-08-17 08:42:21 +0000

    Aug 17, 2025, 8:42 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,12 +1,12 @@
 // write your codes here
-let shapeColor='blue';
-let cicleSize =5;
-let rectSize=50;
-function setup(){
-    createCanvas(400,400);
-    background(220);
-    noStroke();
-}
+// let shapeColor='blue';
+// let cicleSize =5;
+// let rectSize=50;
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     noStroke();
+// }
 
 // function mousePressed(){
 //     shapeColor=color(random(255),random(255),random(255));
@@ -20,7 +20,21 @@ function setup(){
 // }
 
 // 
-function draw(){
+function setup(){
+    createCanvas(400,400);
     background(220);
     
+
 }
+let numCircles=0;
+let ranSize = 0;
+function keyPressed(){
+    numCircles=random(5,20);
+    ranSize=random(10,80);
+    for (let i = 0;i<numCircles;i++){
+        fill(random(255),random(255),random(255));
+        ellipse(random(400),random(400),ranSize,ranSize);
+
+    }
+}
+
