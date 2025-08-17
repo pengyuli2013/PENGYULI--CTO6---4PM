2da92a9b78302e6317f51ec83db1bbb1bdb80445
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-08-17 08:48:03 +0000

    Aug 17, 2025, 8:48 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -31,7 +31,7 @@ let ranSize = 0;
 let keytime=1;
 function keyPressed(){
     numCircles=random(5,20);
-
+    
     for (let i = 0;i<numCircles;i++){
         ranSize=random(10,80);
         fill(random(255),random(255),random(255));
