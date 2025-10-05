9aa0572293ae317854dc04efa150394bdef35b22
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-10-05 08:57:17 +0000

    Oct 5, 2025, 8:57 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,26 +1,32 @@
 let countdown = 5;
 let interval;
 let change;
+let sound;
 let bgColor = 220;
+
+function preload(){
+    sound=loadSound('assets/bossaNova.mp3');
+}
+
 function setup(){
     createCanvas(600,400);
-    textAllign(CENTRE,CENTRE);
+    textAlign(CENTER,CENTER);
 
 }
 
-function draw (){
+function draw() {
     background(bgColor);
     textSize(24);
-    text("Click to start timer",width/2,height-50);
+    text("Click to start timer", width/2, height-50);
     textSize(80);
-    text(countdown,width/2,height/2);
+    text(countdown, width/2, height/2);
 }
 
 function mousePressed(){
     interval=setInterval(updateCountdown,1000);
 }
 
-function draw(){
+function updateCountdown(){
     if(countdown>0){
         countdown-=1;
 
@@ -28,5 +34,17 @@ function draw(){
     }else{
         clearInterval(interval);
         change=setInterval(changeVar,500);
+    }
+}
+
+function changeVar(){
+    bgColor = color(random(255),random(255),random(255));
+    sound.play();
+}
+
+function keyPressed(){
+    if(keyCode===32){
+        clearInterval(change);
+        sound.stop();
     }
 }
