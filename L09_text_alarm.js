dd6f5450df085795d009ec45b5e868864070b427
pengyuli2013 <pengyuli2013.sg@gmail.com>
2025-10-05 08:39:34 +0000

    Oct 5, 2025, 8:39 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,1 +1,32 @@
-// write your codes here
+let countdown = 5;
+let interval;
+let change;
+let bgColor = 220;
+function setup(){
+    createCanvas(600,400);
+    textAllign(CENTRE,CENTRE);
+
+}
+
+function draw (){
+    background(bgColor);
+    textSize(24);
+    text("Click to start timer",width/2,height-50);
+    textSize(80);
+    text(countdown,width/2,height/2);
+}
+
+function mousePressed(){
+    interval=setInterval(updateCountdown,1000);
+}
+
+function draw(){
+    if(countdown>0){
+        countdown-=1;
+
+
+    }else{
+        clearInterval(interval);
+        change=setInterval(changeVar,500);
+    }
+}
